84f59216c02ef4ec2e2a6a5b2ded7a0bfcf0e4b1
Justin Morante <38294724+justinmorante@users.noreply.github.com>
2024-02-04 13:33:28 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
-                            QuartzCart
+                            CallvilleCart
                         </Link>
                     </li>
                 </ul>
